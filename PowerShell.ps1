7d41d6e093adabddd9e585b17692b7e077f5bb21
Phoenixtekk <lacy@phoenixtekk.io>
2022-11-15 12:35:29 -0700

    Renamed ps file

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -2,7 +2,7 @@
 	.NOTES
 	===========================================================================
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.211
-	 Created on:   	11/14/2022 12:53 PM
+	 Created on:   	11/15/2022 8:07 AM
 	 Created by:   	Lacy
 	 Organization: 	Phoenixtekk
 	 Filename:     	
@@ -16,4 +16,3 @@ param ([string]$message = "Hello World"
 )
 
 Write-Output $message
-
